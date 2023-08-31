86998dee9bd19edf8397aa3d8b54f18f53684e90
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-31 09:59:46 +0200

    1st test passed

--- src/snakesAndLadders.js
+++ src/snakesAndLadders.js
@@ -1,5 +1,26 @@
 function snakesAndLadders(board, dice) {
-  return 'snakesAndLadders';
+
+
+  // board = [0,0,3,0,0,0,0,-2,0,0,0] 
+  // dice = [2,1,5,1,5,4]
+
+  let currentField = 0;
+
+  dice.forEach(diceValue => {
+    console.log('Current field:', currentField)
+    console.log('Dice value: ', diceValue)
+    if (currentField + diceValue < board.length){
+      currentField += diceValue;
+      console.log('Current field after throw:', currentField)
+      if(board[currentField] !== 0 ) {
+        console.log('There was a value on the board, value: ', board[currentField])
+        currentField += board[currentField];
+      }
+    }
+  });
+
+
+  return currentField;
 }
 
 module.exports = snakesAndLadders;
